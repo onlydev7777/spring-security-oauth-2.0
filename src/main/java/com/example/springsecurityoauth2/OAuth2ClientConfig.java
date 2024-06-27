bc8e04d2b772f31909dfa2e870682d5bb91ba546
onlydev7777 <onlydev7777@gmail.com>
2024-06-27 12:21:29 +0900

    - OAuth 2.0 로그아웃 핸들러 설정

--- src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,23 +1,41 @@
 package com.example.springsecurityoauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class OAuth2ClientConfig {
+  private final ClientRegistrationRepository clientRegistrationRepository;
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests()
-//        .antMatchers("/loginPage").permitAll()
+//        .antMatchers("/login/loginPage").permitAll()
         .anyRequest().authenticated()
         .and()
         .oauth2Login(Customizer.withDefaults());
+
+    http.logout(logout->logout
+        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+        .invalidateHttpSession(true)
+        .clearAuthentication(true)
+        .deleteCookies("JSESSIONID")
+    );
+
 //        .oauth2Login(login->login.loginPage("/loginPage"));
     return http.build();
   }
 
+  private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+    OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+    successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+    return successHandler;
+  }
 }
