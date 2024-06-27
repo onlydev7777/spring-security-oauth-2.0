e71bcde9c93c89f44c0e40fb0326d4a1b1b8e136
onlydev7777 <onlydev7777@gmail.com>
2024-06-27 12:25:20 +0900

    - Spring MVC에서 인증 객체 참조

--- src/main/java/com/example/springsecurityoauth2/LoginController.java
+++ src/main/java/com/example/springsecurityoauth2/LoginController.java
@@ -1,12 +1,42 @@
 package com.example.springsecurityoauth2;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/login")
 @RestController
 public class LoginController {
   @GetMapping("/loginPage")
   public String loginPage(){
     return "loginPage";
+  }
+
+  @GetMapping("/user")
+  public OAuth2User user(Authentication authentication){
+    OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
+    OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+    OAuth2User oAuth2User2 = oAuth2AuthenticationToken.getPrincipal();
+    System.out.println("oAuth2User = " + oAuth2User);
+    System.out.println("oAuth2User2 = " + oAuth2User2);
+    return oAuth2User;
+  }
+
+  @GetMapping("/oauth2User")
+  public OAuth2User oauth2User(@AuthenticationPrincipal OAuth2User oAuth2User){
+    System.out.println("oAuth2User = " + oAuth2User);
+    return oAuth2User;
+  }
+
+  @GetMapping("/oidcUser")
+  public OidcUser oidcUser(@AuthenticationPrincipal OidcUser oidcUser){
+    System.out.println("oidcUser = " + oidcUser);
+    return oidcUser;
   }
 }
