ac28017e649a4b86dcac30e14c0b0900835bcbd3
onlydev7777 <onlydev7777@gmail.com>
2024-07-02 12:24:03 +0900

    - OAuth2 Social Login   1) OAuth2ClientConfig      : userInfoEndpoint 설정        - customOAuth2UserService        - customOidcUserService      : /api/user, /api/oidc 접근권한 설정        - SCOPE_profile, SCOPE_email        - SCOPE_openid

--- src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,62 +1,37 @@
 package com.example.springsecurityoauth2;
 
-import com.example.springsecurityoauth2.filter.CustomOAuth2LoginAuthenticationFilter;
+import com.example.springsecurityoauth2.service.CustomOAuth2UserService;
+import com.example.springsecurityoauth2.service.CustomOidcUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class OAuth2ClientConfig {
-  private final ClientRegistrationRepository clientRegistrationRepository;
-  private final DefaultOAuth2AuthorizedClientManager authorizedClientManager;
-  private final OAuth2AuthorizedClientRepository authorizedClientRepository;
+
+  private final CustomOAuth2UserService customOAuth2UserService;
+  private final CustomOidcUserService customOidcUserService;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests()
-        .antMatchers("/login/loginPage", "/oauh2Login", "/client").permitAll()
-        .anyRequest().authenticated()
-        .and()
-//        .oauth2Login(Customizer.withDefaults());
-    .oauth2Client(Customizer.withDefaults());
+    http.authorizeRequests((requests) -> requests
+        .antMatchers("/static/js/**", "/static/images/**", "/static/css/**", "/static/scss/**").permitAll()
+        .antMatchers("/").permitAll()
+        .antMatchers("/api/user").access("hasAnyRole('SCOPE_profile','SCOPE_email')")
+        .antMatchers("/api/oidc").access("hasRole('SCOPE_openid')")
+        .anyRequest().authenticated());
 
-    http.addFilterBefore(customOAuth2LoginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
-//    http.logout(logout->logout
-//        .logoutSuccessHandler(oidcLogoutSuccessHandler())
-//        .invalidateHttpSession(true)
-//        .clearAuthentication(true)
-//        .deleteCookies("JSESSIONID")
-//    );
-
-//        .oauth2Login(login->login.loginPage("/loginPage"));
-
+    http.oauth2Login(oauth2 -> oauth2
+        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+            .userService(customOAuth2UserService)
+            .oidcUserService(customOidcUserService)
+        )
+    );
+    http.logout().logoutSuccessUrl("/");
 
     return http.build();
-  }
-
-  private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-    OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-    successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-    return successHandler;
-  }
-
-  public CustomOAuth2LoginAuthenticationFilter customOAuth2LoginAuthenticationFilter() throws Exception {
-    CustomOAuth2LoginAuthenticationFilter customOAuth2LoginAuthenticationFilter =
-        new CustomOAuth2LoginAuthenticationFilter(authorizedClientManager, authorizedClientRepository);
-    customOAuth2LoginAuthenticationFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
-      response.sendRedirect("/home");
-    });
-    return customOAuth2LoginAuthenticationFilter;
   }
 }
